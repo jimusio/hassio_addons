f1db83157384eb811b2845d5064b26084a61a4e2
SchumyHao <schumyhaojl@126.com>
2018-10-19 18:23:00 +0800

    seperate config data and user data
    Signed-off-by: SchumyHao <schumyhaojl@126.com>

--- kr/run.sh
+++ kr/run.sh
@@ -5,13 +5,15 @@ CLOUDUSERNAME=$(jq --raw-output ".cloud_username" $CONFIG_PATH)
 CLOUDPASSWORD=$(jq --raw-output ".cloud_password" $CONFIG_PATH)
 LISTENPORT=$(jq --raw-output ".listen_port" $CONFIG_PATH)
 
-CLOUD_CONFIG_FILE=/data/.config/cloud.json
-USER_CONFIG_FILE=/data/.config/config.json
+CLOUD_CONFIG_FILE=./.config/cloud.json
+USER_CONFIG_FILE=./.config/config.json
 
 jq -n -c -M --arg name "$CLOUDUSERNAME" --arg passwd "$CLOUDPASSWORD" '{"username":$name,"password":$passwd}' > $CLOUD_CONFIG_FILE
 if [ $LISTENPORT ]; then
   jq -n -c -M --argjson v $LISTENPORT '{"listen": $v}' > $USER_CONFIG_FILE
 fi
 
+export KR_DATA_PATH='/data'
+
 # Startup
-./kr -P '/data/'
+./kr
